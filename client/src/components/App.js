577f68f89cd6a7dd84aefd54dded1e8c91761cb3
Anand S <anand.santhosh@trovapage.com>
2020-06-24 01:14:02 +0530

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Header from "./header/Headers";
+import Landing from "./landing/Landing";
+import "./styles.css";
+class App extends Component {
+  componentDidMount() {}
+
+  render() {
+    
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Switch>
+              <Route path="/" component={Landing} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+        <div className="footer">
+          Built by{" "}
+          <a target="_blank" href="">
+            Anand
+          </a>© 2020
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server/config/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://anands:anands@cluster0-vvebg.mongodb.net/url_shortner?retryWrites=true&w=majority",
+  errorUrl: "https://www.google.com"
+};
